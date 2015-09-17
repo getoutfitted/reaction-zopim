3ed761a696a7cfaec9e3ee626054b27265141d4c
Paul Grever <paulgrever@gmail.com>
2015-09-17 10:24:26 -0600

    working on second test

--- tests/jasmine/server/zopim.js
+++ tests/jasmine/server/zopim.js
@@ -1,8 +1,13 @@
 
-describe('Meteor Zopim global methods', function() {
+describe('Meteor Zopim methods', function() {
    
-    it('Zopim status', function() {
+    it('Zopim initial status is disabled', function() {
       var zopim = Meteor.Zopim.isEnabled();
       expect(zopim).toBe(false);
     });
+    it('Zopim has no chatwidget initially', function() {
+      var zopim = Meteor.Zopim.accountOptions(); 
+      expect(zopim).toEqual("hi");
+    });
+
 });
